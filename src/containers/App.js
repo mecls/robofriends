ee91fe272d81050646c47ec69a10e2f502a34218
flopt04 <zakusenshi@gmail.com>
2023-05-29 16:29:21 +0100

    Error precautions made

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React,{Component}  from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import './App.css';
+import ErrorBoundry from "../components/ErrorBoundry";
 import Scroll from '../components/Scroll';
+import './App.css';
 
 class App extends Component{
     constructor() {
@@ -37,7 +38,9 @@ class App extends Component{
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/>
             <Scroll>
-            <CardList robots={filteredRobots} />
+              <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+              </ErrorBoundry>
             </Scroll>
           </div>
         );
